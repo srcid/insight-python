3803a9a9c00686267548ee657b728a4019048830
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2024-06-09 19:21:07 -0300

    using string for uvicorn

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from uvicorn import run
 
-from insight_python.app import app
 from insight_python.settings import settings
 
 if __name__ == "__main__":
     if settings.FASTAPI_PRODUCTION:
-        run(app, host="0.0.0.0", port=80, reload=False)
+        run("insight_python.app:app", host="0.0.0.0", port=9999, reload=False)
     else:
         run(
             "insight_python.app:app",
