3f87c5d47296894d433d6af03665d27409fcb4c0
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2024-06-09 19:25:04 -0300

    get port from port variable

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from insight_python.settings import settings
 
 if __name__ == "__main__":
     if settings.FASTAPI_PRODUCTION:
-        run("insight_python.app:app", host="0.0.0.0", port=9999, reload=False)
+        run("insight_python.app:app", host="0.0.0.0", port=settings.PORT, reload=False)
     else:
         run(
             "insight_python.app:app",
